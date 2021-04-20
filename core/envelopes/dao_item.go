d3c0c9a402d474486fc558f7ed4612b37766ede5
gouzuoluo <1046292959@qq.com>
2021-04-20 15:51:56 +0800

    修复读取红包的BUG

--- core/envelopes/dao_item.go
+++ core/envelopes/dao_item.go
@@ -34,7 +34,7 @@ func (dao *RedEnvelopeItemDao) Insert(form *RedEnvelopeItem) (int64, error) {
 func (dao *RedEnvelopeItemDao) FindItems(envelopeNo string) []*RedEnvelopeItem {
 	items := make([]*RedEnvelopeItem, 0)
 	sql := "select * from red_envelope_item where envelope_no=?"
-	err := dao.runner.Find(items, sql, envelopeNo)
+	err := dao.runner.Find(&items, sql, envelopeNo)
 	if err != nil {
 		logrus.Error(err)
 		return items
